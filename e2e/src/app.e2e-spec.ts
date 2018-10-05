d03ebe44b29b20c5972bf659d4445453825ff8d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-10-05 15:30:51 +0200

    ok

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { element, by, browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -9,6 +10,20 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to essai!');
+    expect(page.getParagraphText()).toEqual('header works!');
+  });
+
+  it('should enter a password and see it in clear', () => {
+    page.navigateTo();
+    const input = element(by.css('input[type=password]'));
+    input.clear();
+    input.sendKeys('azerty');
+    browser.sleep(2000);
+    const button = element(by.css('fa-icon'));
+    button.click();
+    browser.sleep(2000);
+    const input2 = element(by.css('input[type=text]'));
+    console.log('input2', input2);
+    expect(input2.getAttribute('value')).toEqual('azerty');
   });
 });
